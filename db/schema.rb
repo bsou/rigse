84820219888dca03a70746985a254ca1872f38b9
Jason Simpson <jsimpson@concord.org>
2017-09-26 13:55:04 -0700

    Schema update.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20170914202111) do
+ActiveRecord::Schema.define(:version => 20170921190628) do
 
   create_table "access_grants", :force => true do |t|
     t.string   "code"
